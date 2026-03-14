c98b76b20f74d21fddcdebed7a898887073a8f79
Pablo Vergés <hello@pablo.codes>
2026-03-14 23:34:13 +0100

    Add the navigation to the app

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <header>
+    <h1>BOUBA</h1>
+    <Navigation :routes="router.options.routes" />
+  </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navigation from "@/components/Navigation.vue";
+import router from "@/router";
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from "@/pages/Home.vue";
+import NeuHier from "@/pages/NeuHier.vue";
+import Bouldern from "@/pages/Bouldern.vue";
+import Trainieren from "@/pages/Trainieren.vue";
+import Spielen from "@/pages/Spielen.vue";
+import Geniessen from "@/pages/Geniessen.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/neu-hier", component: NeuHier, name: "Neu Hier" },
+  { path: "/bouldern", component: Bouldern, name: "Bouldern" },
+  { path: "/trainieren", component: Trainieren, name: "Trainieren" },
+  { path: "/spielen", component: Spielen, name: "Spielen" },
+  { path: "/geniessen", component: Geniessen, name: "Geniessen" },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes,
+});
 
-export default router
+export default router;
