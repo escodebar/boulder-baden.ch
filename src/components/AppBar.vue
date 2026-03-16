2c35e97f751d90201b4817f4ec4d6b44f9ed898a
Pablo Vergés <hello@pablo.codes>
2026-03-16 17:00:51 +0100

    Display the nav in the AppBar on non-mobile devices

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -14,4 +14,28 @@ import Navigation from "@/components/Navigation.vue";
 import router from "@/router";
 </script>
 
-<style scoped></style>
+<style scoped>
+@media (min-width: 768px) {
+  button[command="show-modal"] {
+    display: none;
+  }
+
+  dialog#nav-dialog {
+    display: block;
+    position: static;
+    border: none;
+    padding: 0;
+    margin: 0;
+    background: transparent;
+    overflow: visible;
+  }
+
+  dialog#nav-dialog nav ul {
+    display: flex;
+  }
+
+  button[command="close"] {
+    display: none;
+  }
+}
+</style>
