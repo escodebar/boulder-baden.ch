1b02f5e3f4c8f38975c622f3a00e8a95cd5ae225
Pablo Vergés <hello@pablo.codes>
2026-03-16 14:09:15 +0100

    Split styles into desktop, mobile and tablet
    Change the name of global, to avoid sorting the imports to break the
    styling, since all files are combined into one and _global contains the
    :root values, which need to be set before they're used.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import "@/styles/global.css";
+import "@/styles/_global.css";
+import "@/styles/desktop.css";
+import "@/styles/mobile.css";
+import "@/styles/tablet.css";
 import App from "@/App.vue";
 import router from "@/router";
 import { createApp } from "vue";
