c08eaaca697384af59069a60785da2a48277a739
Pablo Vergés <hello@pablo.codes>
2026-03-27 08:12:47 +0100

    Change and vertically center header logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -4,12 +4,13 @@
 
 <style scoped>
 h1 {
-  background-image: url("@/assets/logo.svg");
+  background-image: url("@/assets/logo-small.svg");
   background-repeat: no-repeat;
   background-size: contain;
   display: block;
-  height: var(--logo-height);
-  width: var(--logo-width);
+  margin: auto 0;
+  height: 44px;
+  width: 219px;
   text-indent: -9999px;
   overflow: hidden;
   white-space: nowrap;
