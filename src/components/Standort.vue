271aeabfbe0509e02a97b779097287ba2d9b53a8
Pablo Vergés <hello@pablo.codes>
2026-04-12 07:21:10 +0200

    Add Map

--- src/components/Standort.vue
+++ src/components/Standort.vue
@@ -1,4 +1,10 @@
 <template>
   <h2>Standort</h2>
   <p>Knapp 10 min vom Bahnhof Baden und Stadtzentrum zu Fuss entfernt</p>
+
+  <Map />
 </template>
+
+<script setup lang="ts">
+import Map from "@/components/Map.vue";
+</script>
