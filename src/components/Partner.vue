657ac92b800fac53c1956b96a8ae8f247c6d4afa
Pablo Vergés <hello@pablo.codes>
2026-04-12 07:51:04 +0200

    Add list of partners
    Logos etc will follow

--- src/components/Partner.vue
+++ src/components/Partner.vue
@@ -1,3 +1,22 @@
 <template>
   <h2>Unsere Partner</h2>
+  <ul v-if="partners.length">
+    <li v-for="partner in partners" :key="partner.name">
+      <a :href="partner.link">
+        {{ partner.name }}
+      </a>
+    </li>
+  </ul>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import partnersData from "@/assets/partners.json";
+
+interface Partner {
+  name: string;
+  link: string;
+}
+
+const partners = ref<Partner[]>(partnersData as Partner[]);
+</script>
