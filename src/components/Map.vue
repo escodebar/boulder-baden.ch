38b488e7ac8c3a0314950d82c5166ae140bd1ca3
Pablo Vergés <hello@pablo.codes>
2026-04-12 07:21:10 +0200

    Add Map

---
+++ src/components/Map.vue
@@ -0,0 +1,16 @@
+<template>
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2697.0686115649164!2d8.302825276542988!3d47.46909239746287!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47906dc1bd11d99f%3A0xc732b05bf43f46db!2sBOUBA%20Boulder%20Baden!5e0!3m2!1sen!2sch!4v1775971076857!5m2!1sen!2sch"
+    allowfullscreen=""
+    loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade"
+  ></iframe>
+</template>
+
+<style scoped>
+iframe {
+  border: none;
+  width: 600px;
+  height: 450px;
+}
+</style>
