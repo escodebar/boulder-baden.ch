2c9f3a9ed5dbc453f3e87868fc1a9f9fa65feb49
Pablo Vergés <hello@pablo.codes>
2026-05-04 14:26:41 +0200

    Add Partners as SVGs

--- src/components/Partner.vue
+++ src/components/Partner.vue
@@ -1,8 +1,12 @@
 <template>
   <h2>Unsere Partner</h2>
   <ul v-if="partners.length">
-    <li v-for="partner in partners" :key="partner.name">
-      <a :href="partner.link">
+    <li v-for="partner in partners" :key="partner.id">
+      <a
+        :class="partner.id"
+        :href="partner.link"
+        :style="`background-image: url(partners/${partner.id}.svg)`"
+      >
         {{ partner.name }}
       </a>
     </li>
@@ -14,9 +18,41 @@ import { ref } from "vue";
 import partnersData from "@/assets/partners.json";
 
 interface Partner {
+  id: string;
   name: string;
   link: string;
 }
 
 const partners = ref<Partner[]>(partnersData as Partner[]);
 </script>
+
+<style scoped>
+ul {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 50px;
+  justify-items: center;
+  align-items: center;
+  padding: 1em;
+  list-style: none;
+}
+
+li {
+  width: 100%;
+  height: auto;
+  position: relative;
+}
+
+a {
+  display: block;
+  height: 100%;
+  width: 100%;
+  aspect-ratio: 3/2;
+  background-size: contain;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  text-indent: -9999px;
+  overflow: hidden;
+  white-space: nowrap;
+}
+</style>
