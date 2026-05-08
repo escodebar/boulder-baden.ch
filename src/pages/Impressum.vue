a3cce80e8cf797ffccc1b908404bf85f088e8e52
Pablo Vergés <hello@pablo.codes>
2026-05-08 18:00:35 +0200

    Add content Impressum

--- src/pages/Impressum.vue
+++ src/pages/Impressum.vue
@@ -1,1 +1,74 @@
-<template></template>
+<template>
+  <h1>Impressum</h1>
+
+  <section>
+    <address>
+      Bouba AG<br />
+      Oberstadtstrasse 12<br />
+      5400 Baden<br />
+      <br />
+
+      <a href="tel:+41566101717">+41 (0)56 610 17 17</a><br />
+      <a href="mailto:info@boulder-baden.ch">info@boulder-baden.ch</a><br />
+      <a href="https://boulder-baden.ch">https://boulder-baden.ch</a>
+    </address>
+
+    <h3>Vertretungsberechtigte Personen</h3>
+    <ul>
+      <li>Kevin Huser</li>
+      <li>Dimitri Canonica</li>
+    </ul>
+  </section>
+
+  <section>
+    <h3>Urheberrecht</h3>
+    <p>
+      Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder
+      anderen Dateien auf der Website gehören ausschliesslich der Firma Bouba AG
+      oder den speziell genannten Rechtsinhabern. Für die Reproduktion jeglicher
+      Elemente ist die schriftliche Zustimmung der Urheberrechtsträger im Voraus
+      einzuholen.
+    </p>
+
+    <h3>Bildquellen</h3>
+    <dl>
+      <dt>Logo-Design</dt>
+      <dd>Yves Krähenbühl</dd>
+
+      <dt>Portrait-Bilder</dt>
+      <dd>Tinus Meier</dd>
+
+      <dt>Bilder Gebäude</dt>
+      <dd>Peter Huser, Nadja Pfister</dd>
+    </dl>
+  </section>
+
+  <section>
+    <h3>Haftungsausschluss</h3>
+    <p>
+      Die Inhalte dieser Website werden mit grösstmöglicher Sorgfalt erstellt.
+      Dennoch übernimmt die Bouba AG keine Gewähr für die Richtigkeit,
+      Vollständigkeit und Aktualität der bereitgestellten Informationen.
+    </p>
+    <p>
+      Die Bouba AG behält sich ausdrücklich vor, Inhalte jederzeit ohne
+      Ankündigung zu ändern, zu ergänzen oder zu entfernen.
+    </p>
+  </section>
+
+  <section>
+    <h3>Datenschutz</h3>
+    <p>
+      Informationen zur Bearbeitung personenbezogener Daten finden Sie in
+      <RouterLink :to="{ name: 'Datenschutz' }">
+        unserer Datenschutzerklärung.
+      </RouterLink>
+    </p>
+  </section>
+</template>
+
+<style scoped>
+* {
+  text-align: left;
+}
+</style>
