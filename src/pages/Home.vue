8bfa35d122587668b93ac4bb4c2598c0d94a6bcb
Pablo Vergés <hello@pablo.codes>
2026-05-29 16:51:16 +0200

    Remove non-scoped styling from home
    ...since it messes up all the other styles

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -32,38 +32,3 @@ import Partner from "@/components/Partner.vue";
 import Standort from "@/components/Standort.vue";
 import Vision from "@/components/Vision.vue";
 </script>
-
-<style>
-address,
-dt,
-dd,
-p {
-  font-size: var(--font-size-text-big);
-}
-
-@media (max-width: 767px) {
-  h2 {
-    font-size: clamp(
-      var(--font-size-text-big),
-      calc(500vw / 64),
-      var(--font-size-h1-mobile)
-    );
-  }
-  h3 {
-    font-size: var(--font-size-h2-mobile);
-  }
-}
-
-@media (min-width: 768px) {
-  h2 {
-    font-size: clamp(
-      var(--font-size-h1-tablet),
-      calc(100vw / 12),
-      var(--font-size-h1-desktop)
-    );
-  }
-  h3 {
-    font-size: var(--font-size-h2-desktop);
-  }
-}
-</style>
