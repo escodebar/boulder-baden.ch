e3612115ae2b6a962676da68ca69b72027fa4829
Pablo Vergés <hello@pablo.codes>
2026-05-29 15:55:52 +0200

    Add Schwierigkeitsgrade

--- src/pages/Bouldern.vue
+++ src/pages/Bouldern.vue
@@ -1,1 +1,30 @@
-<template></template>
+<template>
+  <h1>On the wall</h1>
+
+  <section>
+    <p>
+      Bei uns findest du um die 200 Boulder auf 1000 m^2 Boulderfläche.
+      Zusätzlich findest du im BOUBA ein Kilter- und Moonboard, eine moderne
+      Spraywall und eine separate Kinderwand für den Nachwuchs.
+    </p>
+  </section>
+
+  <section>
+    <h2>Schwierigkreitsgrade</h2>
+
+    <hr />
+
+    <p>
+      Unsere Schwierigkeits-Skala besteht aus den Graden 1 bis 6. In der
+      Übersicht unten ist die Übersetzung in die Fontainebleau-Skala
+      ersichtlich. Der Schwierigkeitsgrad ist eine subjektive Wahrnehmung eines
+      Boulders. Hast du den Eindruck, dass wir uns bei einem Boulder mit der
+      Schwierigkeit völlig vertan haben? Dann gib uns ein Feedback dazu auf der
+      <a href="https://www.8a.nu/gyms/boulder-baden/topos/bouldering">
+        Vertical-Life App
+      </a>
+      . Je mehr Rückmeldungen wir erhalten, desto genauer können wir die Grade
+      der Boulder anschreiben. Danke für deine Mithilfe.
+    </p>
+  </section>
+</template>
