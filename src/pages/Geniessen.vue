5553120543b97d5abd7ecb427cfb862b12ef4e45
Pablo Vergés <hello@pablo.codes>
2026-05-30 09:36:45 +0200

    Add Geniessen

--- src/pages/Geniessen.vue
+++ src/pages/Geniessen.vue
@@ -1,1 +1,25 @@
-<template></template>
+<template>
+  <h1>Trunk und Speiss</h1>
+
+  <section>
+    <h2>Bouba Bistro</h2>
+
+    <hr />
+
+    <p>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+      sit amet.
+    </p>
+
+    <RouterLink class="button" :to="{ name: 'Öffnungszeiten' }">
+      Öffnungszeiten
+    </RouterLink>
+  </section>
+</template>
