1550b33dbe78ef71982d32f5b1e0d52d917ebe8f
Pablo Vergés <hello@pablo.codes>
2026-06-03 16:19:03 +0200

    Reduce the amount of sections in Firmenangebot

--- src/pages/Firmenangebot.vue
+++ src/pages/Firmenangebot.vue
@@ -1,51 +1,37 @@
 <template>
   <h1>Firmenangebot</h1>
 
-  <section>
-    <p>
-      Sport, gutes Essen und schöne Momente zusammen. Das erwartet euch bei
-      einem Firmenevent im BOUBA.
-    </p>
+  <p>
+    Sport, gutes Essen und schöne Momente zusammen. Das erwartet euch bei einem
+    Firmenevent im BOUBA.
+  </p>
 
-    <p>
-      Zuerst werdet ihr als Gruppe mit einer Leitperson professionell ins
-      Bouldern eingeführt. Falls jemand nicht bouldern möchte, haben wir auch
-      noch zwei Ping-Pong Tische, einen Billardtisch sowie eine Dartscheibe.
-    </p>
-    <p>
-      Nach dem Bouldern lasst ihr es euch gut gehen auf unserer Indoor-Terrasse
-      oder nach Wunsch in unserem Garten. Der Bereich wird für exklusiv für euch
-      reserviert.
-    </p>
-  </section>
+  <p>
+    Zuerst werdet ihr als Gruppe mit einer Leitperson professionell ins Bouldern
+    eingeführt. Falls jemand nicht bouldern möchte, haben wir auch noch zwei
+    Ping-Pong Tische, einen Billardtisch sowie eine Dartscheibe.
+  </p>
+  <p>
+    Nach dem Bouldern lasst ihr es euch gut gehen auf unserer Indoor-Terrasse
+    oder nach Wunsch in unserem Garten. Der Bereich wird für exklusiv für euch
+    reserviert.
+  </p>
 
   <section>
     <h3>Dauer</h3>
     <p>Auf Kundenwunsch. Wir empfehlen 1.5 Stunden Bouldern.</p>
-  </section>
-
-  <section>
     <h3>Preis</h3>
     <p>Auf Anfrage</p>
-  </section>
-
-  <section>
     <h3>Apéro und Catering</h3>
     <p>
       Gerne organisieren wir für euch ein Apéro und/oder ein ganzes Catering,
       das auf eure Wünsche zugeschnitten ist.
     </p>
-  </section>
-
-  <section>
     <h3>Selbstständiges Weiterbouldern nach dem Kurs</h3>
     <p>
       Alle, die noch nicht genug haben, dürfen nach der Einführung gerne noch
       weiterbouldern.
     </p>
-  </section>
-
-  <section>
     <h3>Buchung</h3>
     <p>
       Bitte sendet eure Anfrage mit der Anzahl Teilnehmer*innen und dem
