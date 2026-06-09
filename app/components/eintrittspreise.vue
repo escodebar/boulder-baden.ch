f0eb9f7133f69b8ab88481b42b4eb94e551ba58d
Pablo Vergés <hello@pablo.codes>
2026-06-09 16:30:21 +0200

    Refactor and style table

--- app/components/eintrittspreise.vue
+++ app/components/eintrittspreise.vue
@@ -1,75 +1,128 @@
 <template>
   <table>
-    <caption>
-      Eintrittspreise 2026
-    </caption>
-    <thead>
-      <tr>
-        <th></th>
-        <th scope="col">Erwachsene</th>
-        <th scope="col">Ermässigt</th>
-        <th scope="col">Jugend</th>
-        <th scope="col">Kinder</th>
-        <th scope="col">Mini</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">Einzeleintritt</th>
-        <td>24</td>
-        <td>20</td>
-        <td>20</td>
-        <td>14</td>
-        <td>10</td>
-      </tr>
-      <tr>
-        <th scope="row">12-er Abo</th>
-        <td>264</td>
-        <td>220</td>
-        <td>220</td>
-        <td>154</td>
-        <td>110</td>
-      </tr>
-      <tr>
-        <th scope="row">Zweijahresabo</th>
-        <td>1720</td>
-        <td>1320</td>
-        <td>1200</td>
-        <td>820</td>
-        <td></td>
-      </tr>
-      <tr>
-        <th scope="row">Jahresabo</th>
-        <td>940</td>
-        <td>730</td>
-        <td>630</td>
-        <td>440</td>
-        <td></td>
-      </tr>
-      <tr>
-        <th scope="row">Halbjahresabo</th>
-        <td>710</td>
-        <td>560</td>
-        <td>470</td>
-        <td>340</td>
-        <td></td>
-      </tr>
-      <tr>
-        <th scope="row">3 Monate</th>
-        <td>370</td>
-        <td>320</td>
-        <td>260</td>
-        <td>200</td>
-        <td></td>
-      </tr>
-      <tr>
-        <th scope="row">1 Monat</th>
-        <td>185</td>
-        <td>145</td>
-        <td>125</td>
-        <td>90</td>
-        <td></td>
-      </tr>
-    </tbody>
+    <template v-if="inverted">
+      <thead>
+        <tr>
+          <th></th>
+          <th v-for="abo in Abos" scope="col">
+            {{ abo }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="stufe in Alterstufen">
+          <th scope="row">{{ stufe }}</th>
+          <td v-for="abo in Abos">{{ eintrittspreise[stufe]?.[abo] }}</td>
+        </tr>
+      </tbody>
+    </template>
+    <template v-else>
+      <thead>
+        <tr>
+          <th></th>
+          <th v-for="stufe in Alterstufen" scope="col">
+            {{ stufe }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="abo in Abos">
+          <th scope="row">{{ abo }}</th>
+          <td v-for="stufe in Alterstufen">
+            {{ eintrittspreise[stufe]?.[abo] }}
+          </td>
+        </tr>
+      </tbody>
+    </template>
   </table>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+  inverted?: boolean;
+}>();
+
+const Alterstufen = ref([
+  "Erwachsene",
+  "Ermässigt",
+  "Jugend",
+  "Kinder",
+  "Minis",
+]);
+
+const Abos = ref([
+  "Einzeleintritt",
+  "12-er Abo",
+  "Zweijahresabo",
+  "Jahresabo",
+  "Halbjahresabo",
+  "3 Monate",
+  "1 Monat",
+]);
+
+const eintrittspreise = ref({
+  Erwachsene: {
+    Einzeleintritt: "24",
+    "12-er Abo": "264",
+    Zweijahresabo: "1720",
+    Jahresabo: "940",
+    Halbjahresabo: "710",
+    "3 Monate": "370",
+    "1 Monat": "185",
+  },
+  Ermässigt: {
+    Einzeleintritt: "20",
+    "12-er Abo": "220",
+    Zweijahresabo: "1320",
+    Jahresabo: "730",
+    Halbjahresabo: "560",
+    "3 Monate": "320",
+    "1 Monat": "145",
+  },
+  Jugend: {
+    Einzeleintritt: "20",
+    "12-er Abo": "220",
+    Zweijahresabo: "1200",
+    Jahresabo: "630",
+    Halbjahresabo: "470",
+    "3 Monate": "260",
+    "1 Monat": "125",
+  },
+  Kinder: {
+    Einzeleintritt: "14",
+    "12-er Abo": "154",
+    Zweijahresabo: "820",
+    Jahresabo: "440",
+    Halbjahresabo: "340",
+    "3 Monate": "200",
+    "1 Monat": "90",
+  },
+  Minis: {
+    Einzeleintritt: "10",
+    "12-er Abo": "100",
+    Zweijahresabo: "",
+    Jahresabo: "",
+    Halbjahresabo: "",
+    "3 Monate": "",
+    "1 Monat": "",
+  },
+});
+</script>
+
+<style scoped>
+table {
+  border-collapse: separate;
+  border-spacing: 1em;
+}
+
+thead th {
+  text-align: right;
+}
+
+th {
+  text-align: left;
+}
+td {
+  text-align: right;
+}
+</style>
