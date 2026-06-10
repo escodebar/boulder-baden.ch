11779940603312689320128cbde4c06586d57f48
Pablo Vergés <hello@pablo.codes>
2026-06-10 19:32:02 +0200

    Improve Öffnungszeiten

--- app/pages/oeffnungszeiten.vue
+++ app/pages/oeffnungszeiten.vue
@@ -1,8 +1,16 @@
 <template>
-  <h2>Öffnungszeiten</h2>
+  <h1>Öffnungszeiten</h1>
+
+  <p>
+    Egal ob nach der Arbeit am späten Abend oder am Wochenende mit der Familie:
+    Wir sind für dich da, wenn du Zeit zum Bouldern hast. Unsere Öffnungszeiten
+    sind flexibel gestaltet, damit du dich entspannt auf die Matte begeben
+    kannst, wann immer es dir passt.
+  </p>
 
   <section>
-    <h3>Reguläre Öffnungzeiten</h3>
+    <h2>Reguläre Öffnungzeiten</h2>
+
     <dl>
       <dt>Montag bis Freitag</dt>
       <dd>11 bis 23</dd>
@@ -12,7 +20,7 @@
   </section>
 
   <section>
-    <h3>Besondere Öffnungszeiten</h3>
+    <h2>Besondere Öffnungszeiten</h2>
 
     <dl>
       <template v-for="holiday in holidays">
@@ -41,3 +49,33 @@ definePageMeta({
   title: "Öffnungszeiten",
 });
 </script>
+
+<style scoped>
+dl {
+  display: grid;
+  grid-template-columns: minmax(0, 1fr) auto;
+  width: fit-content;
+  max-width: 100%;
+  margin-inline: auto;
+  gap: 0.25rem 1rem;
+}
+
+dt {
+  margin: 0;
+}
+
+dd {
+  margin: 0;
+  text-align: right;
+}
+
+dt::after {
+  content: ":";
+}
+
+@media (max-width: 767px) {
+  h1 {
+    font-size: clamp(var(--font-size-h2), calc(27vw / 2), var(--font-size-h1));
+  }
+}
+</style>
