4617cf0865ce3e0d9fd05c7e3b6ff298f3b395ed
Pablo Vergés <hello@pablo.codes>
2026-06-10 19:31:41 +0200

    Improve Hallenregeln

--- app/pages/hallenregeln.vue
+++ app/pages/hallenregeln.vue
@@ -2,33 +2,38 @@
   <h1>Hallenregeln</h1>
 
   <p>
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-    voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-    amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-    diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-    amet.
+    Wir sind hier, um Spass zu haben und gemeinsam zu wachsen. Damit jeder hier
+    glücklich bouldert, sicher fällt und gute Laune hat, gibt es ein paar
+    einfache Regeln. Sie dienen nicht dazu, euch zu ärgern, sondern um
+    sicherzustellen, dass die Matte für alle da ist und dass niemand auf dem
+    Kopf landet.
   </p>
 
   <section id="sicherheit">
     <h2>Sicherheit</h2>
+
+    <p>
+      Hier bouldern wir hart, aber sicher. Damit niemand verletzt wird und wir
+      alle lange Spass haben, gelten ein paar einfache Grundsätze. Es geht nicht
+      darum, euch einzuschränken, sondern darum, dass die Halle immer ein
+      sicherer Ort bleibt, für euch und alle anderen.
+    </p>
+
     <ul>
-      <li>Vor dem Bouldern gut aufwärmen</li>
       <li>Wer bereits an der Wand bouldert, hat Vorrang</li>
-      <li>Top-Out verboten</li>
+      <li>Wärme dich vor dem Bouldern gut auf</li>
+      <li>Renne nicht auf den Matten</li>
+      <li>Top-Out ist nicht erlaubt</li>
     </ul>
   </section>
 
   <section id="sauberkeit">
     <h2>Rücksicht & Sauberkeit</h2>
     <ul>
-      <li>Immer aufeinander Rücksicht nehmen</li>
-      <li>Verschmutze Griffe mit der Bürste reinigen</li>
-      <li>Barfuss bouldern ist verboten</li>
-      <li>Spass haben 🙂</li>
+      <li>Verteile nicht unnötig Magnesia in der Luft</li>
+      <li>Reinige verschmutze Griffe mit der Bürste</li>
+      <li>Nehmt immer Rücksicht aufeinander</li>
+      <li>Klettere nicht Barfuss</li>
     </ul>
   </section>
 
@@ -59,5 +64,11 @@ definePageMeta({
 <style scoped>
 li {
   text-align: left;
+}
+
+@media (max-width: 767px) {
+  h1 {
+    font-size: clamp(var(--font-size-h2), calc(65vw / 4), var(--font-size-h1));
+  }
 }
 </style>
