9fc6ba9ff9625083dd315e35affc73e1b0cddd4f
Pablo Vergés <hello@pablo.codes>
2026-06-11 12:15:16 +0200

    Hide bottom navigation in mobile view
    ...since the navigation is already easily accessible in the bottom app
    bar.

--- app/app.vue
+++ app/app.vue
@@ -3,8 +3,16 @@
   <main>
     <NuxtPage />
 
-    <Navigation />
+    <Navigation class="bottom-nav" />
 
     <Footer />
   </main>
 </template>
+
+<style scoped>
+@media (max-width: 767px) {
+  .bottom-nav {
+    display: none;
+  }
+}
+</style>
