d53f9d9292a74532965d078af2f61e35b5222963
Pablo Vergés <hello@pablo.codes>
2026-06-18 21:07:10 +0200

    Make textes left aligned on mobile

--- app/components/kontakt.vue
+++ app/components/kontakt.vue
@@ -3,9 +3,8 @@
     Willst du etwas wissen? Oder hast du sonst eine Anfrage? Lass es uns
     wissen:<br />
     <a href="mailto:info@boulder-baden.ch">info@boulder-baden.ch</a><br />
-    <a href="tel:+41566101717">+41 (0)56 610 17 17</a>
-  </address>
-  <p>
+    <a href="tel:+41566101717">+41 (0)56 610 17 17</a><br />
+    <br />
     Bleibe up to date, folge uns auf
     <a
       class="icon instagram"
@@ -13,7 +12,7 @@
     >
       Instagram
     </a>
-  </p>
+  </address>
 </template>
 
 <style scoped>
