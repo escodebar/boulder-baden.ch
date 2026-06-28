6b5b4182487e219177909dc9071625207521ae0e
Pablo Vergés <hello@pablo.codes>
2026-06-28 10:54:07 +0200

    Replace bereiche png with webp

--- app/components/bereich.vue
+++ app/components/bereich.vue
@@ -4,24 +4,23 @@
 
 <style scoped>
 div {
-  aspect-ratio: 721/641;
+  aspect-ratio: 721/640;
 }
 
 div.gesamt {
-  background-image: url("@/assets/bereiche/gesamt.png");
-  aspect-ratio: 721/640;
+  background-image: url("@/assets/bereiche/gesamt.webp");
 }
 div.bouldern {
-  background-image: url("@/assets/bereiche/bouldern.png");
+  background-image: url("@/assets/bereiche/bouldern.webp");
 }
 div.spielen {
-  background-image: url("@/assets/bereiche/spielen.png");
+  background-image: url("@/assets/bereiche/spielen.webp");
 }
 div.trainieren {
-  background-image: url("@/assets/bereiche/trainieren.png");
+  background-image: url("@/assets/bereiche/trainieren.webp");
 }
 div.geniessen {
-  background-image: url("@/assets/bereiche/geniessen.png");
+  background-image: url("@/assets/bereiche/geniessen.webp");
 }
 
 @media (max-width: 767px) {
